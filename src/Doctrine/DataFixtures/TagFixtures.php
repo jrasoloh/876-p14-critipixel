79858907d3ecfb91674c416bcbe460effb2a8eb0
jossy.rasoloharijaona <jossy.rasoloharijaona@openclassrooms.com>
2026-09-16 18:10:54 +0200

    feat: add tags and reviews fixtures for video games

---
+++ src/Doctrine/DataFixtures/TagFixtures.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Doctrine\DataFixtures;
+
+use App\Model\Entity\Tag;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+use function array_map;
+use function array_walk;
+
+final class TagFixtures extends Fixture
+{
+    private const NAMES = [
+        'Action',
+        'Aventure',
+        'RPG',
+        'Stratégie',
+        'Simulation',
+        'Sport',
+        'Course',
+        'Puzzle',
+        'Plateforme',
+        'Tir',
+        'Horreur',
+        'Combat',
+        'Multijoueur',
+        'Indépendant',
+        'Rythme',
+    ];
+
+    public function load(ObjectManager $manager): void
+    {
+        $tags = array_map(
+            static fn (string $name): Tag => (new Tag())->setName($name),
+            self::NAMES
+        );
+
+        array_walk($tags, [$manager, 'persist']);
+
+        $manager->flush();
+    }
+}
+
